463d8b5e410e69343f6ea2fd0f603fdf78a93864
Jake Overall <joverall22@gmail.com>
2018-08-27 11:59:32 -0600

    vslap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 :class="{green: !dead, red: dead}">
+      {{target.name}}
+      {{target.health}}
+      {{target.items}}
+    </h1>
+    <div class="attacks">
+      <button 
+      v-for="(value, key) in target.attacks"
+      :key="key"
+      @click="attack(value)"
+      :disabled="dead"
+      >{{key}}</button>
+      <button @click="reset" v-if="dead">Reset</button>
+    </div>
+    <div class="items">
+      <button class="item" 
+      v-for="item in availableItems" 
+      :key="item.id"
+      @click="giveItem(item)"
+      >{{item.name}}</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      target: {
+        health: 100,
+        name: "ScarCrow",
+        attacks: {
+          kick: 10,
+          slap: 1,
+          punch: 5,
+          haduouken: 100
+        },
+        items: []
+      },
+      availableItems: [
+        {
+          id: 1,
+          name: "Shield",
+          modifier: 0.2
+        }
+      ]
+    };
+  },
+  methods: {
+    attack(val) {
+      this.target.health -= val;
+    },
+    reset() {
+      this.target.health = 100;
+    },
+    giveItem(item) {
+      if (this.target.items.includes(item)) {
+        return;
+      }
+      this.target.items.push(item);
+    }
+  },
+  computed: {
+    dead() {
+      return this.target.health <= 0;
+    }
   }
-}
+};
 </script>
+
+<style>
+.red {
+  background-color: red;
+  color: white;
+}
+.green {
+  background-color: green;
+  color: white;
+}
+</style>
+
